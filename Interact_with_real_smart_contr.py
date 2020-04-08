ba19b5865b8361bf796751ae6b04a717d5fc4915
pablomendez95 <mendez4a@gmail.com>
2020-04-08 14:05:05 +0200

    Done first transaction between two accounts

--- Interact_with_real_smart_contr.py
+++ Interact_with_real_smart_contr.py
@@ -8,11 +8,48 @@ abi = json.loads('[{"inputs":[{"internalType":"address","name":"","type":"addres
 
 # address of the deployed smart contract
 address_contract = "0x9DE5F2CAa47CD11a8FD097DD396754474d7f95FB"
+address_account_1 = "0x61D38805c04C8cb9b5D71bdAfaD874Fa2ac091D3"
+address_account_2 = "0x4B165B929Aa5120e6848a9d0d742faD0929e80b5"
+private_key_1 = "817F2FE20B21A70670937550E3F83209DFB3C80E6C0BD6C08DDA3DD7A3F665AF"
 
 contract = web3.eth.contract(address = address_contract, abi = abi)
 
 variable = contract.functions.getBalance().call()
-balance = web3.eth.getBalance("0x4B165B929Aa5120e6848a9d0d742faD0929e80b5")
+balance_1 = web3.eth.getBalance(address_account_1)
+balance_2 = web3.eth.getBalance(address_account_2)
 
-print(web3.fromWei(balance, "ether"))
+print("The balance of account 1 is: ", web3.fromWei(balance_1, "ether"))
+print("The balance of account 2 is: ", web3.fromWei(balance_2, "ether"))
 print(variable)
+
+nonce = web3.eth.getTransactionCount(address_account_1)
+
+tx = {
+    'nonce': nonce,
+    'to': address_account_2,
+    'value': web3.toWei(0.01, 'ether'),
+    'gas': 30000,
+    'gasPrice': web3.toWei('50', 'gwei')
+}
+
+signed_tx = web3.eth.account.signTransaction(tx, private_key_1)
+tx_hash = web3.eth.sendRawTransaction(signed_tx.rawTransaction)
+print(web3.toHex(tx_hash))
+
+'''w3 = Web3(HTTPProvider("https://ropsten.infura.io/v3/YOUR_PROJECT_ID"))
+
+contract_ = w3.eth.contract(
+    abi=contract_interface['abi'],
+    bytecode=contract_interface['bin'])
+
+acct = w3.eth.account.privateKeyToAccount(privateKey)
+
+construct_txn = contract_.constructor().buildTransaction({
+    'from': acct.address,
+    'nonce': w3.eth.getTransactionCount(acct.address),
+    'gas': 1728712,
+    'gasPrice': w3.toWei('21', 'gwei')})
+
+signed = acct.signTransaction(construct_txn)
+
+w3.eth.sendRawTransaction(signed.rawTransaction)'''
