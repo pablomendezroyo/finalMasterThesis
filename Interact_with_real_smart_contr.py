09d9ca71f02148dbffa7ee6dd2f1a2f0dac06c23
pablomendez95 <mendez4a@gmail.com>
2020-04-13 20:35:21 +0200

    added set buyer functionality and set money needed for transaction

--- Interact_with_real_smart_contr.py
+++ Interact_with_real_smart_contr.py
@@ -4,8 +4,8 @@ infura_url = "https://goerli.infura.io/v3/21eb6a6c17fc415eb9f4cf08d91f9f93"
 web3 = Web3(Web3.HTTPProvider(infura_url))
 
 # CONTRACT
-abi = json.loads('[	{		"inputs": [			{				"internalType": "address payable",				"name": "_seller",				"type": "address"			},			{				"internalType": "uint256",				"name": "_amount_min_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_amount_max_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_price_min_kwh",				"type": "uint256"			}		],		"name": "matchBuyer",		"outputs": [],		"stateMutability": "nonpayable",		"type": "function"	},	{		"anonymous": false,		"inputs": [			{				"indexed": false,				"internalType": "bool",				"name": "_status",				"type": "bool"			}		],		"name": "matchFound",		"type": "event"	},	{		"inputs": [			{				"internalType": "address",				"name": "_buyer",				"type": "address"			},			{				"internalType": "uint256",				"name": "_amount_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_price_max_kwh",				"type": "uint256"			}		],		"name": "matchSeller",		"outputs": [],		"stateMutability": "nonpayable",		"type": "function"	},	{		"inputs": [],		"name": "sendMoney",		"outputs": [],		"stateMutability": "payable",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "_amount_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_price_max_kwh",				"type": "uint256"			}		],		"name": "setBuyer",		"outputs": [],		"stateMutability": "nonpayable",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "_amount_min_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_amount_max_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_price_min_kwh",				"type": "uint256"			}		],		"name": "setSeller",		"outputs": [],		"stateMutability": "nonpayable",		"type": "function"	},	{		"inputs": [			{				"internalType": "address",				"name": "",				"type": "address"			}		],		"name": "balanceReceived",		"outputs": [			{				"internalType": "uint256",				"name": "totalBalance",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "numPayments",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "address",				"name": "",				"type": "address"			}		],		"name": "buyer",		"outputs": [			{				"internalType": "address",				"name": "addressBuyer",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_max_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [],		"name": "getBalance",		"outputs": [			{				"internalType": "uint256",				"name": "",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "",				"type": "uint256"			}		],		"name": "Offers_Buyer_Array",		"outputs": [			{				"internalType": "address",				"name": "addressBuyer",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_max_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "",				"type": "uint256"			}		],		"name": "Offers_Seller_Array",		"outputs": [			{				"internalType": "address payable",				"name": "addressSeller",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_min_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "amount_max_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_min_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "address",				"name": "",				"type": "address"			}		],		"name": "seller",		"outputs": [			{				"internalType": "address payable",				"name": "addressSeller",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_min_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "amount_max_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_min_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	}]')
-address_contract = "0xdBc26FfeFd95b0F864C401F9216A30344c1b2e65"
+abi = json.loads('[	{		"anonymous": false,		"inputs": [			{				"indexed": false,				"internalType": "bool",				"name": "_status",				"type": "bool"			}		],		"name": "matchFound",		"type": "event"	},	{		"inputs": [],		"name": "sendMoney",		"outputs": [],		"stateMutability": "payable",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "_amount_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_price_max_kwh",				"type": "uint256"			}		],		"name": "setBuyer",		"outputs": [],		"stateMutability": "nonpayable",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "_amount_min_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_amount_max_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "_price_min_kwh",				"type": "uint256"			}		],		"name": "setSeller",		"outputs": [],		"stateMutability": "nonpayable",		"type": "function"	},	{		"inputs": [			{				"internalType": "address",				"name": "",				"type": "address"			}		],		"name": "balanceReceived",		"outputs": [			{				"internalType": "uint256",				"name": "totalBalance",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "numPayments",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "address",				"name": "",				"type": "address"			}		],		"name": "buyer",		"outputs": [			{				"internalType": "address",				"name": "addressBuyer",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_max_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "_amountInWei",				"type": "uint256"			}		],		"name": "convertWeiToEther",		"outputs": [			{				"internalType": "uint256",				"name": "",				"type": "uint256"			}		],		"stateMutability": "pure",		"type": "function"	},	{		"inputs": [],		"name": "getBalance",		"outputs": [			{				"internalType": "uint256",				"name": "",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "",				"type": "uint256"			}		],		"name": "Offers_Buyer_Array",		"outputs": [			{				"internalType": "address",				"name": "addressBuyer",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_max_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "uint256",				"name": "",				"type": "uint256"			}		],		"name": "Offers_Seller_Array",		"outputs": [			{				"internalType": "address payable",				"name": "addressSeller",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_min_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "amount_max_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_min_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	},	{		"inputs": [			{				"internalType": "address",				"name": "",				"type": "address"			}		],		"name": "seller",		"outputs": [			{				"internalType": "address payable",				"name": "addressSeller",				"type": "address"			},			{				"internalType": "uint256",				"name": "amount_min_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "amount_max_kw",				"type": "uint256"			},			{				"internalType": "uint256",				"name": "price_min_kwh",				"type": "uint256"			}		],		"stateMutability": "view",		"type": "function"	}]')
+address_contract = "0xC32Bd490C5399867E50Aac9dbd072ad4D43E2890"
 
 # ACCOUNTS
 address_account_1 = "0x61D38805c04C8cb9b5D71bdAfaD874Fa2ac091D3"
@@ -35,17 +35,17 @@ def set_Seller():
 def set_Money(amount):
     nonce = web3.eth.getTransactionCount(address_account_1)
 
-    transaction_sendMoney = contract.functions.sendMoney().buildTransaction({
+    construct_txn = contract.functions.sendMoney().buildTransaction({
             'gas': 1000000,
             'gasPrice': web3.toWei('1', 'gwei'),
             'value': web3.toWei(amount, 'ether'),
-            'from': address_account_2,
+            'from': address_account_1,
             'nonce': nonce
         })
-    print(transaction_sendMoney)
-    signed_txn = web3.eth.account.signTransaction(transaction_sendMoney, private_key=private_key_1)
-    web3.eth.sendRawTransaction(signed_txn.rawTransaction)
-    web3.waitForTransactionReceipt(signed_txn)
+    print(construct_txn)
+    signed_txn = web3.eth.account.signTransaction(construct_txn, private_key=private_key_1)
+    tx_hash = web3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    tx_receipt = web3.eth.waitForTransactionReceipt(tx_hash)
     print(signed_txn)
     return signed_txn
 
@@ -68,8 +68,8 @@ def set_Buyer(amount_kw, price_max_kwh):
 
 # PRINT BALANCES
 def get_balance():
-    variable = contract.functions.getBalance().call()
-    print(variable)
+    balance_contract = contract.functions.getBalance().call()
+    print(balance_contract)
     balance_1 = web3.eth.getBalance(address_account_1)
     #balance_2 = web3.eth.getBalance(address_account_2)
 
@@ -79,9 +79,20 @@ def get_balance():
     #print("The balance of account 2 is: ", web3.fromWei(balance_2, "ether"))
     return balance_1_ether
 
+def get_balance_received():
+    balance_received_list = contract.functions.balanceReceived(address_account_1).call()
+    #print(balance_received)
+    balance_received_wei = balance_received_list[0]
+    balance_received_ether = web3.fromWei(balance_received_wei, "ether")
+    num_payments = balance_received_list[1]
+    #web3.fromWei(balance_received, "ether")
+    return balance_received_ether
+
 def main(): 
-    balance = get_balance()
+    balance = get_balance_received()
     print(balance)
+    #insertion = set_Money(0.01)
+    #print(insertion)
 
 if __name__ == '__main__':
     main()
--- buyer.py
+++ buyer.py
@@ -1,11 +1,11 @@
 from request import Request
-from Interact_with_real_smart_contr import set_Money, get_balance
+from Interact_with_real_smart_contr import set_Money, get_balance_received
 
 class Buyer:
 
     # batteryLevel
     def __init__(self, battery_Level):
-        self.amount_kw =self.set_Amount()
+        self.amount_kw =self.set_amount_kw()
         self.price_max_kwh = self.set_Price()
         self.balance = self._get_balance()
 
@@ -17,26 +17,22 @@ class Buyer:
         actual_price = request.price
         return actual_price + 2
 
-    def set_Amount(self):
-        amount = 2
+    def set_amount_kw(self):
+        amount = 0.002
         return amount
 
     def _get_balance(self):
-        balance = get_balance()
+        balance = get_balance_received()
         print("current balance: ", balance)
-        balance = update_balance(balance)
-        print("updated balance: ",update_balance)
-        return balance
-
-    def update_balance(self, balance):
+        print(self.amount_kw*self.price_max_kwh)
         if(balance < self.amount_kw*self.price_max_kwh):
-            set_Money(self.amount_kw*self.price_max_kwh - balance)
-            updated_balance = get_balance()
+            set_Money(self.amount_kw*self.price_max_kwh - float(balance))
+            updated_balance = get_balance_received()
+            print("updated balance: ",updated_balance)
             return updated_balance
         elif(balance >= self.amount_kw*self.price_max_kwh):
+            print("current balance: ", balance)
             return balance
-
-
 
 def main():
     buyer = Buyer(1)
